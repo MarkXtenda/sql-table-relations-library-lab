bede1b46ea82343b9d8e62b86178d99d1e8da9c8
MarkXtenda <mark.i2p.1998@gmail.com>
2022-08-22 16:35:20 +1000

    done

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(SPECIES) AS amount FROM characters GROUP BY species ORDER BY amount DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT a.name, subgenres.name FROM authors AS a INNER JOIN series AS s ON s.author_id = a.id INNER JOIN subgenres ON subgenres.id = s.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title FROM series INNER JOIN authors AS a ON a.id = series.author_id INNER JOIN characters ON characters.author_id = a.id WHERE species = 'human' GROUP BY species ORDER BY COUNT(species) DESC;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.title) AS amount  FROM characters INNER JOIN character_books ON character_id = characters.id INNER JOIN books ON books.id = book_id GROUP BY characters.name ORDER BY amount DESC, characters.name ASC;"
 end
